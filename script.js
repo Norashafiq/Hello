e57afdcc60619c2f2d4335a17ada5216aaf9aa7e
Nora Almutahar <noura.shafiq@gmail.com>
2022-06-05 13:56:14 +0300

    please

---
+++ script.js
@@ -0,0 +1,17 @@
+let addTaskButton = document.getElementById("addTask");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+addTaskButton.addEventListener("click", function () {
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener("click", function () {
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener("dblclick", function () {
+        toDoContainer.removeChild(paragraph);
+    })
+})
